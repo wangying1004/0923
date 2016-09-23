b47dac7f20f01502349ddd031a7eb79f8943e76b
wangying <wwwww@mail.com>
2016-09-23 14:01:09 +0800

    全部

---
+++ 1.js
@@ -0,0 +1,18 @@
+(function () {
+	// var btn1 = document.getElementById('btn1');
+	var bt =document.getElementsByTagName('button');
+	var bt1 =document.getElementsByTagName('button')[0];
+	var bt2 =document.getElementsByTagName('button')[1];
+	var bt3 =document.getElementsByTagName('button')[2];
+	var bt4 =document.getElementsByTagName('button')[3];
+    
+    var btnname1 = document.getElementsByName('btnname1');
+
+    var btnname1 = document.getElementsByName('btnname1');
+
+    var btnclass = document.getElementsByClassName('btnclass');
+    btnclass.onclick  = function(){
+    	console.log('btnclass');
+    }
+}(window))
+
